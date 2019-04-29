98a41839c6dcbeb4773cd4db64ff82c42349eff8
JoonSooHa <json.ha@gmail.com>
2019-04-30 05:11:31 +0900

    update

--- ultra96/EX15/menu_example.c
+++ ultra96/EX15/menu_example.c
@@ -2,37 +2,36 @@
 
 int main()
 {
-	char c;
-	char str[100];
+    char c;
+    char str[100];
 
-	while(1) {
-		printf("==== Menu Option ====\n\r");
-		printf("a. first menu\n\r");
-		printf("b. second menu\n\r");
-		printf("s. enter string\n\r");
-		printf("x. finish\n\r");
-		printf("Select Menu : ");
-		c = getchar();
+    while(1) {
+        printf("==== Menu Option ====\n\r");
+        printf("a. first menu\n\r");
+        printf("b. second menu\n\r");
+        printf("s. enter string\n\r");
+        printf("x. finish\n\r");
+        printf("Select Menu : ");
+        c = getchar();
 
-		printf("==> Selected Menu : %c\n\r", c);
+        printf("==> Selected Menu : %c\n\r", c);
 
-		if ((c != 'a') && (c != 'b') && (c != 's') && (c != 'x')) {
-			printf("WRONG COMMAND\n\r");
-		}
+        if ((c != 'a') && (c != 'b') && (c != 's') && (c != 'x')) {
+            printf("WRONG COMMAND\n\r");
+        }
 
-		if (c == 's') {
-			printf("enter string : ");
-			scanf("%s", str);
-			getchar();
-			printf("\nstring is %s\n\r", str);
-		}
+        if (c == 's') {
+            printf("enter string : ");
+            scanf("%s", str);
+            getchar();
+            printf("\nstring is %s\n\r", str);
+        }
 
-		if (c == 'x') {
-			break;
-		}
-	}
+        if (c == 'x') {
+            break;
+        }
+    }
 
-	printf("program finished\n\r");
+    printf("program finished\n\r");
     return 0;
 }
-

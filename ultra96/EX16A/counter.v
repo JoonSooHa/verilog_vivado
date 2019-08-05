174119545e4c4c2481b4a48a27180ebdbbde71b7
JoonSooHa <json.ha@gmail.com>
2019-08-05 20:27:32 +0900

    fix bug

--- ultra96/EX16A/counter.v
+++ ultra96/EX16A/counter.v
@@ -3,32 +3,28 @@ module counter(
     input rst,
     input clr,
     input en,
-    output reg[26:0] cnt,
-    output[7:0] q,
+    output reg q,
     output reg cnt_max
 ); 
 
     always @(posedge clk)
     begin
         if(rst | clr)
-            cnt <= 27'd0;
+            q <= 27'd0;
         else if(en)
         begin
-            if(cnt == 27'd99_999_999)
-                cnt <= 27'd0;
+            if(q == 27'd99_999_999)
+                q <= 27'd0;
             else
-                cnt <= cnt + 1;
+                q <= q + 1;
         end
     end
     
-    assign q = cnt[26:19];
-    
     always @(*)
     begin
-        if(cnt == 27'd99_999_999)
+        if(q == 27'd99_999_999)
             cnt_max <= 1'b1;
         else
             cnt_max <= 1'b0;
     end
-
 endmodule
